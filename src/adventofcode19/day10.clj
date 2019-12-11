08511cb77422cca9bda79eb8c233e4603b369d61
Mario Cordova <mario.cordova@pnmac.com>
2019-12-10 17:57:48 -0800

    Refactored day10

--- src/adventofcode19/day10.clj
+++ src/adventofcode19/day10.clj
@@ -2,59 +2,37 @@
   (:require [clojure.string :as str]
             [clojure.math.combinatorics :as combo]))
 
-(def input )
+(def input (slurp "/Users/mcordova/Projects/adventofcode19/src/adventofcode19/inputs/day10.txt"))
 
 (def grid (mapv #(str/split % #"") (str/split-lines input)))
 
-(count grid)
+(def asteroids
+  (->> (for [row (range (count grid))
+             col (range (count grid))
+             :when (= "#" (get-in grid [row col]))]
+         [col row])
+       (remove nil?)
+       (into [])))
 
 
-(def asteroids (remove nil? (for [row (range 33)
-                                  col (range 33)]
-                              (if (= "#" (get-in grid [row col]))
-                                [col row]))))
+(defn create-angle
+  [[x1 y1] [x2 y2]]
+  (let [dy (- y2 y1)
+        dx (- x2 x1)
+        angle (Math/toDegrees (Math/atan2 dy dx))]
+    (+ angle (* (Math/ceil (/ (- angle) 360)) 360))))
 
-(count asteroids)
+(def locations
+  (map (fn [a]
+         [a (count (distinct (for [b asteroids
+                                   :when (not= a b)]
+                               (create-angle a b))))]) asteroids))
 
-(defn line-of-sight?
-  [a b]
-  (let [[x1 y1] a
-        [x2 y2] b]
-    (if (= x2 x1)
-      (fn [x y]
-        (and (= x x2)
-             (or (< y1 y y2)
-                 (> y1 y y2))))
-      (if (= y2 y1)
-        (fn [x y]
-          (and (= y y2)
-               (or (< x1 x x2)
-                   (> x1 x x2))))
-        (let [slope (/ (- y2 y1) (- x2 x1))
-              c (- y2 (* slope x2))]
-          (fn [x y]
-            (and (= y (+ c (* slope x)))
-                 (or (< x1 x x2)
-                     (> x1 x x2))
-                 (or (< y1 y y2)
-                     (> y1 y y2)))))))))
+(defn day10
+  []
+  (last (sort-by second locations)))
+
+(day10)
 
 
-(def results (for [a asteroids
-                   b asteroids]
-               [a (when (not= a b)
-                    (let [los? (line-of-sight? a b)]
-                      (if (empty? (filter true? (map (fn [[x y]]
-                                                       (when (not (or (= [x y] a)
-                                                                      (= [x y] b)))
-                                                         (los? x y))) asteroids)))
-                        1
-                        0)))]))
 
-
-(def day10 (for [astr (partition 303 results)]
-             [(first (first astr))(->> (map second astr)
-                                       (remove nil?)
-                                       (reduce +))]))
-
-(last (sort-by second day10))
